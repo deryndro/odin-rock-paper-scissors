54e0f8ec6f55fbd374f3915e02fd931c131bbc82
deryndro <email>
2024-05-07 20:28:07 +0700

    Adding logic to the game

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * 3);
+  return choices[randomNum];
+}
+
+const playerChoice = prompt("Rock, paper, scissors?").toLowerCase();
+
+// Check if player input is correct
+
+// Compare computers choice vs players choice
