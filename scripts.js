0f1b705af6d976ecba2b59c964b5425c9da78d03
deryndro <email>
2024-05-08 16:22:46 +0700

    Adding score counter and every game plays for 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomNum = Math.floor(Math.random() * 3);
@@ -18,31 +21,46 @@ function getPlayerChoice() {
   }
 }
 
+function winAlert(player, computer) {
+  console.log(`You win! ${player} beats ${computer}`);
+}
+function loseAlert(player, computer) {
+  console.log(`You lose! ${computer} beats ${player}`);
+}
+
 // Compare computers choice vs players choice
 function winner(getComputerChoice, getPlayerChoice) {
   const computerChoice = getComputerChoice();
   const playerChoice = getPlayerChoice();
   if (computerChoice === playerChoice) {
-    alert("TIE");
+    playerScore + 0;
+    computerScore + 0;
   } else if (playerChoice == "rock") {
     if (computerChoice == "paper") {
-      alert("YOU WIN");
+      winAlert(playerChoice, computerChoice);
+      playerScore++;
     } else {
-      alert("YOU LOST");
+      loseAlert(playerChoice, computerChoice);
+      computerScore++;
     }
   } else if (playerChoice == "paper") {
     if (computerChoice == "rock") {
-      alert("YOU WIN");
+      winAlert(playerChoice, computerChoice);
+      playerScore++;
     } else {
-      alert("YOU LOST");
+      loseAlert(playerChoice, computerChoice);
+      computerScore++;
     }
   } else if (playerChoice == "scissors") {
     if (computerChoice == "paper") {
-      alert("YOU WIN");
+      winAlert(playerChoice, computerChoice);
+      playerScore++;
     } else {
-      alert("YOU LOST");
+      loseAlert(playerChoice, computerChoice);
+      computerScore++;
     }
   }
+  console.log(`Player score: ${playerScore} Computer score: ${computerScore}`);
 }
 
-winner(getComputerChoice, getPlayerChoice);
+Array.from({ length: 5 }, () => winner(getComputerChoice, getPlayerChoice));
